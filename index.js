63c6d97461db1aa1a2691d56d0c68f3946b6e73d
Charlene Badibanga <charlenebadibanga@gmail.com>
2017-02-23 21:08:38 +0000

    gettting up to date

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-//document.getElementById("demo").onclick = function() {getMeals()};
-
 function getMeals() {
   document.getElementById("meals").innerHTML = "PLACEHOLDER: Meal suggestions go here";
 }
